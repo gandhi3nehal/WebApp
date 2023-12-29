04322f76cd122b80bc5f0576dd55b68eea14c165
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 05:08:23 -0500

    Create variables.tf

---
+++ dev/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "This is the cloud hosting region where your webapp will be deployed."
+  region      = "us-east-2"
+}
+
+variable "dev_prefix" {
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  dev_prefix  = "dev"
+}
+
+
