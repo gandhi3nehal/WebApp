43efd6e7f5920c3868027631b66a61fc6a25bcf7
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 06:10:07 -0500

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,19 +1,19 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  region      = "us-east-2"
+  default      = "us-east-2"
 }
 
 variable "prod_prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-  prod_prefix = "prod"
+  default = "prod"
 }
 
 variable "db_read_capacity" {
   type = number
-  db_read_capacity = 1
+  default = 1
 }
 
 variable "db_write_capacity" {
   type = number
-  db_write_capacity = 1
+  default = 1
 }
