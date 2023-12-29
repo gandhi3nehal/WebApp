6dc29ca57daf0fdc3ff4d3073f1d8ae46d24bd8a
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 11:10:43 -0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -138,7 +138,7 @@ resource "aws_iam_instance_profile" "webapp_profile" {
 
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-06ca3ca175f37dd66"
-  instance_type = "t2.micro"
+  instance_type = "t2.large"
   
   iam_instance_profile = aws_iam_instance_profile.webapp_profile.name
 
