bdee76f76c41092a45055451c0e762a148d2e788
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 10:59:50 -0500

    Update outputs.tf

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,8 +1,8 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-output "dev_website_endpoint" {
-  value = "http://${aws_s3_bucket_website_configuration.dev.website_endpoint}/index.html"
+output "webapp_bucket_website_endpoint" {
+  value = "http://${aws_s3_bucket_website_configuration.webapp_bucket.website_endpoint}/index.html"
 }
 
 output "environment_table_name" {
