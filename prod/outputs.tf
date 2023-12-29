a4fcb60d8f478f8a0a448bef533823e1a64386cb
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 05:27:04 -0500

    Update outputs.tf

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,3 +1,13 @@
 output "prod_website_endpoint" {
   value = "http://${aws_s3_bucket_website_configuration.prod.website_endpoint}/index.html"
 }
+
+output "environment_table_name" {
+  description = "Name of the environment DynamoDB table"
+  value       = aws_dynamodb_table.table.name
+}
+
+output "environment_table_arn" {
+  description = "ARN of the environment DynamoDB table"
+  value       = aws_dynamodb_table.table.arn
+}
