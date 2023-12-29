93a01cc300f57934c652edf5ac77b2c6392c2de8
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 06:20:01 -0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      Environment = "${var.dev_prefix}"
+      Environment = "${var.env_prefix}"
       Service     = "webapp"
     }
   }
@@ -14,7 +14,7 @@ resource "random_pet" "petname" {
 }
 
 resource "aws_s3_bucket" "dev" {
-  bucket = "${var.dev_prefix}-${random_pet.petname.id}"
+  bucket = "${var.env_prefix}-${random_pet.petname.id}"
 
   force_destroy = true
 }
@@ -95,7 +95,7 @@ resource "aws_s3_object" "dev" {
 resource "random_pet" "table_name" {}
 
 resource "aws_dynamodb_table" "table" {
-  name = "${var.dev_prefix}-${random_pet.table_name.id}"
+  name = "${var.env_prefix}-${random_pet.table_name.id}"
 
   read_capacity  = var.db_read_capacity
   write_capacity = var.db_write_capacity
@@ -108,7 +108,7 @@ resource "aws_dynamodb_table" "table" {
 }
 
 resource "aws_iam_role" "webapp_role" {
-  name = "${var.dev_prefix}-webapp-role"
+  name = "${var.env_prefix}-webapp-role"
 
   assume_role_policy = <<EOF
 {
@@ -132,7 +132,7 @@ resource "aws_iam_role_policy_attachment" "webapp_attachment" {
 }
 
 resource "aws_iam_instance_profile" "webapp_profile" {
-  name = "${var.dev_prefix}-webapp-profile"
+  name = "${var.env_prefix}-webapp-profile"
   role = aws_iam_role.webapp_role.name
 }
 
@@ -143,7 +143,7 @@ resource "aws_instance" "webapp_instance" {
   iam_instance_profile = aws_iam_instance_profile.webapp_profile.name
 
   tags = {
-    Name = "${var.dev_prefix}-webapp-instance"
+    Name = "${var.env_prefix}-webapp-instance"
   }
 }
 
