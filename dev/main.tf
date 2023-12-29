52c8879b32e67a451097e1d1262fcb3feb1b6122
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 05:21:00 -0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -91,3 +91,18 @@ resource "aws_s3_object" "dev" {
   content      = file("${path.module}/assets/index.html")
   content_type = "text/html"
 }
+
+resource "random_pet" "table_name" {}
+
+resource "aws_dynamodb_table" "table" {
+  name = "${var.dev_prefix}-${random_pet.table_name.id}"
+
+  read_capacity  = var.db_read_capacity
+  write_capacity = var.db_write_capacity
+  hash_key       = "UUID"
+
+  attribute {
+    name = "UUID"
+    type = "S"
+  }
+}
