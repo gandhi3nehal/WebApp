de1bf8be474da4c8b7677fa8df284ce8ab5ca931
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-29 06:15:17 -0500

    Update main.tf

--- prod/main.tf
+++ prod/main.tf
@@ -89,7 +89,7 @@ EOF
 resource "aws_s3_object" "prod" {
   key          = "index.html"
   bucket       = aws_s3_bucket.prod.id
-  content      = file("${path.module}/assets/index.html")
+  content      = file("../assets/index.html")
   content_type = "text/html"
 }
 
