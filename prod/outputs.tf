8ec98601a55db6164dd2ac3ba1018ff1d2a021d6
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-01 18:05:40 -0500

    Update outputs.tf

--- prod/outputs.tf
+++ prod/outputs.tf
@@ -1,13 +1,3 @@
-output "webapp_bucket_website_endpoint" {
-  value = "http://${aws_s3_bucket_website_configuration.webapp_bucket.website_endpoint}/index.html"
-}
-
-output "environment_table_name" {
-  description = "Name of the environment DynamoDB table"
-  value       = aws_dynamodb_table.table.name
-}
-
-output "environment_table_arn" {
-  description = "ARN of the environment DynamoDB table"
-  value       = aws_dynamodb_table.table.arn
+output "public_ip" {
+  value = "http://${module.compute.public_ip}"
 }
