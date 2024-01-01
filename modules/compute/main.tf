405c838ba74d939c0050af3ec88a1a972c8dd02d
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-01 15:10:05 -0500

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "app_server" {
   vpc_security_group_ids = [var.sg]
   user_data              = var.user_data
   iam_instance_profile   = var.iam_instance_profile
-  key_name               = data.aws_key_pair.oregon.key_name
+  key_name               = data.aws_key_pair.us-east-2-key.key_name
   tags = {
     Name = "${var.tag_name}instance"
   }
