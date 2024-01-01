d63f498622e3b0a1a91b2080d983d15fd40cad48
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-01 18:06:09 -0500

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -5,15 +5,5 @@ variable "region" {
 
 variable "env_prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-  default = "prod"
-}
-
-variable "db_read_capacity" {
-  type = number
-  default = 2
-}
-
-variable "db_write_capacity" {
-  type = number
-  default = 2
+  default  = "dev"
 }
