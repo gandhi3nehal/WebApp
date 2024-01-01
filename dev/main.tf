f459ebdb930a9bf8cee9a9bee23b7bd92d987082
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-01 15:01:13 -0500

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,25 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
 
 provider "aws" {
   region = "us-west-2"
 }
 
 module "compute" {
-  source               = "./module/compute"
+  source               = "../modules/compute"
   ami                  = "ami-0d70546e43a941d70"
   instance_type        = "t2.micro"
-  tag_name             = "ExampleAppServerInstance"
+  tag_name             = "dev-aws-web-app"
   sg                   = module.security.webserver_sg
-  user_data            = file("./userdata.tpl")
+  user_data            = file("../assets/userdata.tpl")
   iam_instance_profile = module.iam.s3_profile
 }
 
@@ -28,18 +18,18 @@ module "security" {
 }
 
 module "iam" {
-  source                 = "./module/iam"
+  source                 = "../modules/iam"
   role_name              = "s3-list-bucket"
   policy_name            = "s3-list-bucket"
   instance_profile_name  = "s3-list-bucket"
   path                   = "/"
   iam_policy_description = "s3 policy for ec2 to list role"
-  iam_policy             = file("./s3-list-bucket-policy.tpl")
-  assume_role_policy     = file("./s3-list-bucket-trusted-identity.tpl")
+  iam_policy             = file("../assets/s3-list-bucket-policy.tpl")
+  assume_role_policy     = file("../assets/s3-list-bucket-trusted-identity.tpl")
 }
 module "s3" {
-  source        = "./module/s3"
-  bucket_name   = "devinslevelupintechbucket0223"
+  source        = "../modules/s3"
+  bucket_name   = "gnehal-dev-aws-web-app"
   acl           = "private"
   object_key    = "LUIT"
   object_source = "/dev/null"
