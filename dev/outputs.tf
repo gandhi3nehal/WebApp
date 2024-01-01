5298b691e25b891f3aec25ffdd9eb4b7bbaac4e7
Nehal Gandhi <gandhi3nehal@gmail.com>
2023-12-31 19:23:28 -0500

    Update outputs.tf - public ip

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -1,16 +1,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-output "webapp_bucket_website_endpoint" {
-  value = "http://${aws_s3_bucket_website_configuration.webapp_bucket.website_endpoint}/index.html"
-}
-
-output "environment_table_name" {
-  description = "Name of the environment DynamoDB table"
-  value       = aws_dynamodb_table.table.name
-}
-
-output "environment_table_arn" {
-  description = "ARN of the environment DynamoDB table"
-  value       = aws_dynamodb_table.table.arn
+output "public-ip" {
+  value = aws_eip.production-eip.public_ip
 }
