586f04f109884d8e97288858a476b4a929f9e940
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-08 15:20:23 -0500

    Update variables.tf

--- prod/variables.tf
+++ prod/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  default      = "us-east-2"
+  default      = "us-east-1"
 }
 
 variable "env_prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-  default  = "dev"
+  default  = "prod"
 }
