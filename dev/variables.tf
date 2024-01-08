24a45c003c41ae8db8bbf74b2cdf53ceb393baa6
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-08 15:22:42 -0500

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -7,3 +7,8 @@ variable "env_prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
   default  = "dev"
 }
+
+variable "instance_type" {
+  description = "This is the environment where your webapp is deployed. qa, prod, or dev"
+  default  = "t2.micro"
+}
