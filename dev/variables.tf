74ae582ef9d03500ac1f44faaf38f2ffbe21cedd
Nehal Gandhi <gandhi3nehal@gmail.com>
2024-01-08 15:45:13 -0500

    Update variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,14 +1,14 @@
 variable "region" {
   description = "This is the cloud hosting region where your webapp will be deployed."
-  # default      = "us-east-2"
+  default      = "us-east-2"
 }
 
 variable "env_prefix" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-  # default  = "dev"
+  default  = "dev"
 }
 
 variable "instance_type" {
   description = "This is the environment where your webapp is deployed. qa, prod, or dev"
-  # default  = "t2.micro"
+  default  = "t2.micro"
 }
